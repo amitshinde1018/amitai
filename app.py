9d2cf5a1d3afcadf204bcab20a408b275925ba23
Amitshinde1018 <rto.shs@somaniautowheels.com>
2026-02-26 15:13:13 +0530

    fix app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
-import sqlite3, bcrypt
+import sqlite3, bcrypt, os
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
@@ -11,6 +11,8 @@ def init_db():
                  (id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT, role TEXT DEFAULT 'member')''')
     conn.commit()
     conn.close()
+
+init_db()
 
 @app.route('/')
 def home():
@@ -94,5 +96,5 @@ def logout():
     return redirect('/login')
 
 if __name__ == '__main__':
-    init_db()
-   app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
